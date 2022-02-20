f01598258870858f834e16e2a06ab12ddd5b1a75
chulep <chulepik@gmail.com>
2022-02-20 16:30:27 +0300

    Start/Pause/GameOver update 3

---
+++ easyGame/ColorsHelper.swift
@@ -0,0 +1,12 @@
+//
+//  ColorsHelper.swift
+//  easyGame
+//
+//  Created by chulep on 20.02.2022.
+//
+
+import UIKit
+
+struct ColorsHelper {
+    static let gameScreen = CIColor(red: 196, green: 190, blue: 187, alpha: 1)
+}
